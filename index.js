084ed8eea181a7dd941e75b069b1f03280b612e9
Docile123 <imbereyemasoo@gmail.com>
2025-07-15 12:30:46 +0200

    the last commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
-let nextBtn  = document.getAnimations("nextBtn");
+let nextBtn  = document.getElementById("nextBtn");
 
 scrollContainer.addEventListener("wheel",(event)=>{
    event.preventDefault();
    scrollContainer.scrollLeft += event.deltaY;
+    scrollContainer.style.scrollBehavior ="auto";
+});
+
+backBtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft +=900;
+})
+
+nextBtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior ="smooth";
+    scrollContainer.scrollLeft -=900;
 })
